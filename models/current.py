88c36974962234a74e634c06309a308a40bcde39
santhosh karkada <Santhoshkarkada2005@gmail.com>
2024-11-13 17:36:26 +0530

    My commit

---
+++ models/current.py
@@ -0,0 +1,8 @@
+from models.account import Account
+
+class Current(Account):
+    def __init__(self, name, balance, pin_number, privilege,registration_number,website_url):
+        super().__init__(name, balance, pin_number, privilege)
+        self.registration_number=registration_number
+        self.website_url=website_url
+
